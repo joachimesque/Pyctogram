c241ecc8a7806d17d657019f14d70554f4a93be8
Sam <sam@samr1.net>
2018-10-29 18:49:20 +0100

    move app files in one directory

--- pyctogram/config.py
+++ pyctogram/config.py
@@ -5,4 +5,3 @@ database_file =     'database.db'
 default_headers =   {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:62.0) Gecko/20100101 Firefox/62.0'}
 
 elements_per_page = 24
-
