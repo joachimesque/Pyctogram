ce2559587459ccbf6851fd125e23e19522ba3487
Sam <sam@samr1.net>
2018-10-30 22:18:53 +0100

    importer - json file route (WIP)

--- pyctogram/feed/importer.py
+++ pyctogram/feed/importer.py
@@ -1,14 +1,11 @@
 import json
 import os
-
 from flask import (Blueprint, current_app, flash, redirect, render_template,
                    request, url_for)
 from flask_login import current_user, login_required
 from werkzeug.utils import secure_filename
 
-from pyctogram import db
-from pyctogram.helpers.importer import get_account_data
-from pyctogram.model import Account, List
+from pyctogram.helpers.importer import create_accounts
 
 import_blueprint = Blueprint('importer', __name__)
 
@@ -45,65 +42,14 @@ def import_from_json():
                     flash('No contact to import')
                     return redirect(request.url)
 
-                total = 0
                 default_list_info = current_app.config['DEFAULT_LIST_INFO']
                 headers = current_app.config['DEFAULT_HEADERS']
-                contacts_not_ok = []
 
-                for contact in contacts_to_import:
-                    account = Account.query.filter_by(
-                        account_name=contact).first()
-                    if not account:
-                        account_data = get_account_data(contact, headers)
-
-                        if not account_data:
-                            contacts_not_ok.append(account)
-                            continue
-
-                        account = Account(id=account_data["id"],
-                                          account_name=account_data["username"])  # noqa
-                        account.full_name = account_data["full_name"]
-                        account.biography = account_data["biography"]
-                        account.profile_pic_url = account_data[
-                            "profile_pic_url"]
-                        account.profile_pic_url_hd = account_data[
-                            "profile_pic_url_hd"]
-                        account.external_url = account_data["external_url"]
-                        account.external_url_linkshimmed = account_data[
-                            "external_url_linkshimmed"]
-                        account.followed_by = account_data["edge_followed_by"][
-                            "count"]
-                        account.follow = account_data["edge_follow"]["count"]
-                        account.last_updated = 0
-                        account.is_private = int(account_data["is_private"])
-                        db.session.add(account)
-
-                    default_list = List.query.filter_by(
-                        user_id=current_user.id,
-                        shortname=default_list_info['shortname']
-                    ).first()
-
-                    if not default_list:
-                        default_list = List(
-                            user_id=current_user.id,
-                            shortname=default_list_info['shortname'],
-                            longname=default_list_info['longname'],
-                            description=default_list_info['description'],
-                        )
-                        db.session.add(default_list)
-
-                    if account not in current_user.accounts:
-                        current_user.accounts.append(account)
-                        total += 1
-
-                    if account not in default_list.accounts:
-                        default_list.accounts.append(account)
-
-                db.session.commit()
+                total = create_accounts(contacts_to_import, current_user,
+                                        headers, default_list_info)
 
             return redirect(
-                url_for('importer.import_success', import_count=total,
-                        contacts_not_ok=contacts_not_ok))
+                url_for('importer.import_success', import_count=total))
 
     return render_template('import/json.html')
 
--- pyctogram/helpers/importer.py
+++ pyctogram/helpers/importer.py
@@ -3,6 +3,9 @@ import json
 from bs4 import BeautifulSoup
 from requests import get
 
+from pyctogram import db
+from pyctogram.model import Account, List
+
 
 def get_account_data(account_name, headers):
     # This function gets account data from the account_name
@@ -34,3 +37,57 @@ def get_account_data(account_name, headers):
                 exit('Error: could not load account data')
 
     return None
+
+
+def create_accounts(contacts_to_import, current_user, headers, list_info):
+    total = 0
+    for contact in contacts_to_import:
+        account = Account.query.filter_by(
+            account_name=contact).first()
+        if not account:
+            account_data = get_account_data(contact, headers)
+
+            if not account_data:
+                continue
+
+            account = Account(id=account_data["id"],
+                              account_name=account_data["username"])  # noqa
+            account.full_name = account_data["full_name"]
+            account.biography = account_data["biography"]
+            account.profile_pic_url = account_data[
+                "profile_pic_url"]
+            account.profile_pic_url_hd = account_data[
+                "profile_pic_url_hd"]
+            account.external_url = account_data["external_url"]
+            account.external_url_linkshimmed = account_data[
+                "external_url_linkshimmed"]
+            account.followed_by = account_data["edge_followed_by"][
+                "count"]
+            account.follow = account_data["edge_follow"]["count"]
+            account.last_updated = 0
+            account.is_private = int(account_data["is_private"])
+            db.session.add(account)
+
+        default_list = List.query.filter_by(
+            user_id=current_user.id,
+            shortname=list_info['shortname']
+        ).first()
+
+        if not default_list:
+            default_list = List(
+                user_id=current_user.id,
+                shortname=list_info['shortname'],
+                longname=list_info['longname'],
+                description=list_info['description'],
+            )
+            db.session.add(default_list)
+
+        if account not in current_user.accounts:
+            current_user.accounts.append(account)
+            total += 1
+
+        if account not in default_list.accounts:
+            default_list.accounts.append(account)
+
+    db.session.commit()
+    return total
